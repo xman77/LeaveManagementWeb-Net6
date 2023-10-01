fca49f84b697b39b77d006910b0a2b2e52896e6d
xman77 <xman77@gmail.com>
2023-10-01 20:21:39 +0800

    Add ViewModel LeaveTypeVM

---
+++ LeaveManagement.Web/Models/LeaveTypeVM.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagement.Web.Models
+{
+    public class LeaveTypeVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [Display(Name = "Default Number Of Days")]
+        public int DefaultDays { get; set; }
+    }
+}
