e9c7bba810c7ce61cb3515d1517309d1552d25d4
xman77 <xman77@gmail.com>
2023-10-03 12:03:27 +0800

    Using View Models and AutoMapper

---
+++ LeaveManagement.Web/Configurationn/MapperConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using LeaveManagement.Web.Data;
+using LeaveManagement.Web.Models;
+
+namespace LeaveManagement.Web.Configurationn
+{
+    public class MapperConfig:Profile
+    {
+        public MapperConfig()
+        {
+            CreateMap<LeaveType, LeaveTypeVM>().ReverseMap();
+        }
+    }
+}
--- LeaveManagement.Web/Models/LeaveTypeVM.cs
+++ LeaveManagement.Web/Models/LeaveTypeVM.cs
@@ -5,6 +5,7 @@ namespace LeaveManagement.Web.Models
     public class LeaveTypeVM
     {
         public int Id { get; set; }
+        [Display(Name="Leave Type Name")]
         public string Name { get; set; }
         [Display(Name = "Default Number Of Days")]
         public int DefaultDays { get; set; }
