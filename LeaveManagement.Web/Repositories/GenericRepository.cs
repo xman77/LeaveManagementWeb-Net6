7f27fd9584143260f6aca0c84068ee1db738a2c6
xman77 <xman77@gmail.com>
2023-10-06 00:37:34 +0800

    Refactor Controller to Use Repository

--- LeaveManagement.Web/Repositories/GenericRepository.cs
+++ LeaveManagement.Web/Repositories/GenericRepository.cs
@@ -37,8 +37,12 @@ namespace LeaveManagement.Web.Repositories
             return await context.Set<T>().ToListAsync();
         }
 
-        public async Task<T> GetAsync(int id)
+        public async Task<T?> GetAsync(int? id)
         {
+            if (id == null)
+            {
+                return null;
+            }
             return await context.Set<T>().FindAsync(id);
         }
 
