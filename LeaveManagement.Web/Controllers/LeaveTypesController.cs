f685f38dad3c6e2357e2395584f3aed2ebadceed
xman77 <xman77@gmail.com>
2023-10-11 15:49:58 +0800

    Setup Index View

--- LeaveManagement.Web/Controllers/LeaveTypesController.cs
+++ LeaveManagement.Web/Controllers/LeaveTypesController.cs
@@ -128,5 +128,12 @@ namespace LeaveManagement.Web.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AllocateLeave(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
