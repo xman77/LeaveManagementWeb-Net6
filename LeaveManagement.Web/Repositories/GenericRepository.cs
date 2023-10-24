ece61cfade0138bcb51a7c61ec67d15dc59dcf76
xman77 <xman77@gmail.com>
2023-10-24 20:07:19 +0800

    Automate Auditing on Database Records

--- LeaveManagement.Web/Repositories/GenericRepository.cs
+++ LeaveManagement.Web/Repositories/GenericRepository.cs
@@ -55,7 +55,8 @@ namespace LeaveManagement.Web.Repositories
 
         public async Task UpdateAsync(T entity)
         {
-            context.Update(entity);
+            //context.Update(entity);
+            context.Entry(entity).State = EntityState.Modified;
             await context.SaveChangesAsync();
         }
     }
